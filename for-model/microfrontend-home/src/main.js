f98c18c351d6b7cb22f321cddc760bf9de8c0d29
kyusoo <rbtn110@uengine.org>
2023-11-10 15:04:45 +0900

    main.js change

--- for-model/microfrontend-home/src/main.js
+++ for-model/microfrontend-home/src/main.js
@@ -1,99 +1,31 @@
- /*eslint-disable*/
-import Vue from "vue";
-import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
-import router from "./router";
-import Keycloak from 'keycloak-js';
-Vue.config.productionTip = false;
+/* eslint-disable import/order */
+import '@/@iconify/icons-bundle'
+import App from '@/App.vue'
+import vuetify from '@/plugins/vuetify'
+import { loadFonts } from '@/plugins/webfontloader'
+import router from '@/router'
+import '@/styles/styles.scss'
+import '@core/scss/index.scss'
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import axios from 'axios';
+import { Icon } from '@iconify/vue';
 
-import singleSpaVue from "single-spa-vue";
+axios.defaults.baseURL = ''
 
-require('./style.css');
 
-let initOptions = {
-  url: `http://localhost:9090/`,
-  realm: `master`,
-  clientId: `master`,
-  onLoad: `login-required`,
-};
+loadFonts()
 
-let keycloak = new Keycloak(initOptions);
-let useKeycloak = false;
-let vueLifecycles;
+const app = createApp(App)
 
-init();
+app.config.globalProperties.$axios = axios;
 
-function init() {
-  keycloak.init({
-    onLoad: initOptions.onLoad,
-  }).then(auth => {
-    const ONE_MINUTE = 60000;
-  
-    if (!auth) {
-      window.location.reload();
-    } else {
-      console.info(`Auth ok`);
-    }
+app.use(vuetify)
 
-    Vue.prototype.$OAuth = keycloak
+app.use(createPinia())
 
-    useKeycloak = true;
-    
-    window.setTimeout(refreshToken.bind(null, keycloak), ONE_MINUTE);
+app.use(router)
 
-  }).catch(() => {
-    console.error(`Auth Fail`);
-  })
-}
+app.component('Icon',Icon)
 
-if (useKeycloak) {
-  vueLifecycles = singleSpaVue({
-    Vue,
-    appOptions: {
-      vuetify: vuetify,
-      router,
-      render: h => h(App, {
-        props: {
-          OAuth: keycloak,
-        },
-      }),
-    }
-  });
-} else {
-  vueLifecycles = singleSpaVue({
-    Vue,
-    appOptions: {
-      vuetify: vuetify,
-      router,
-      render: h => h(App),
-    }
-  });
-}
-
-function refreshToken() {
-  keycloak.updateToken(70).then(refreshed => {
-    if (refreshed) {
-      successRefresh(refreshed);
-    } else {
-      warnRefresh();
-    }
-  }).error(errorRefresh);
-}
-
-function successRefresh(refreshed) {
-  console.debug(`Token refreshed ${refreshed}`);
-}
-
-function warnRefresh() {
-  console.warn(`Token not refreshed, valid for 
-  ${Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000)} seconds`);
-}
-
-function errorRefresh() {
-  console.error('Failed to refresh token');
-}
-
-export const bootstrap = vueLifecycles.bootstrap;
-export const mount = vueLifecycles.mount;
-export const unmount = vueLifecycles.unmount;
-
+app.mount('#app')
